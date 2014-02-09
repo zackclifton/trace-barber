5e58e5d1d84860123c880f9985c2b937b524ad62
Zack Clifton <ZMClifton@gmail.com>
2014-02-09 13:50:45 -0500

    Bundle of Stuff
    - Went back to the old ShootBullet() function
    
    - Working on ConVars

--- lua/weapons/custompistol.lua
+++ lua/weapons/custompistol.lua
@@ -27,11 +27,28 @@ SWEP.ViewModel			= "models/weapons/v_pistol.mdl"
 SWEP.WorldModel			= "models/weapons/w_pistol.mdl"
 SWEP.UseHands			= false
 
-local shotDamage = 1
+local shotDamage = 50
+local shotRof = 0.5
 local shotCount = 1
-local shotSpread = Vector(0,0,1)
+local shotSpread = 0.07
+
+CreateClientConVar( "cweapon_damage", shotDamage, true, true )
+CreateClientConVar( "cweapon_count", shotCount, true, true )
+CreateClientConVar( "cweapon_spread", shotSpread, true, true )
+CreateClientConVar( "cweapon_rof", shotRof, true, true )
 
 function SWEP:PrimaryAttack()
+	-- For now we're using the built in ShootBullet() function.
+	self.Weapon:SetNextPrimaryFire( CurTime() + GetConVarNumber( "cweapon_rof" ) )
+	self:ShootBullet( GetConVarNumber( "cweapon_damage" ), GetConVarNumber( "cweapon_count" ), GetConVarNumber( "cweapon_spread" ) )
+	
+	-- We'll use CustomShoots() when it's more refined.
+	--CustomShoots(self)
+end
+
+function CustomShoots( self )
+	self:ShootEffects()
+	self.Weapon:EmitSound( "Weapon_AR2.Single" )
 	for var=1,shotCount,1 do
 		--local tr = self.Owner:GetEyeTrace()
 		local tr = util.TraceLine( util.GetPlayerTrace(self.Owner) )
@@ -45,7 +62,13 @@ function SWEP:PrimaryAttack()
 		} )]]--
 	--print( tr.Hit, tr.Entity, iteration )
 	--if !(tr.Entity == self.Owner) then
-		tr.Entity:TakeDamage( shotDamage, self.Owner, self )
+		if ( SERVER ) then
+			if ( tr.HitGroup == HITGROUP_HEAD ) then
+				tr.Entity:TakeDamage( shotDamage*headshotMult, self.Owner, self )
+			else
+				tr.Entity:TakeDamage( shotDamage, self.Owner, self )
+			end			
+		end
 	end
 	--end
 	
@@ -59,7 +82,7 @@ function SWEP:PrimaryAttack()
 	self.Owner:ViewPunch( Angle( 0, 0, 0) )]]--
 end
 
---[[function SWEP:ShootBullet( damage, num_bullets, aimcone )
+function SWEP:ShootBullet( damage, num_bullets, aimcone )
 	
 	local bullet = {}
 
@@ -78,7 +101,7 @@ end
 	
 end
 
-function CustomDamageNPC(npc, hitgroup, dmginfo)
+--[[function CustomDamageNPC(npc, hitgroup, dmginfo)
 	dmginfo:SetDamage(0)
 	if ( hitgroup == HITGROUP_HEAD ) then
 	npc:TakeDamage( shotDamage*1.3, dmginfo:GetAttacker(), dmginfo:GetInflictor() )		
